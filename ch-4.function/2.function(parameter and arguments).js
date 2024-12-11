8dfb86fb81dee486536f6ddcc5df60ef755fb73a
Shivam24S <shivamshrivastav8055@gmail.com>
2024-12-11 19:34:19 +0530

    parameter and arguments

---
+++ ch-4.function/2.function(parameter and arguments).js
@@ -0,0 +1,14 @@
+// parameters and arguments in js function are we can consider like we are sending additional information to our function
+
+// parameters when we invoke our function  with passing some parameters
+
+// example
+
+// here game is argument
+function play(game) {
+  console.log("the game name is " + game);
+}
+
+play("cricket"); // parameter
+
+//as seen above  code we have passed cricket as a parameter to play function
